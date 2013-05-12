e2c3701ddb551d8ff0a9ecc31b69e55c1b6c2b34
Alex Clemmer <clemmer.alexander@gmail.com>
2013-05-12 14:29:53 -0600

    Add sampling package

---
+++ src/fountain_codes/sample.clj
@@ -0,0 +1,34 @@
+(ns fountain-codes.sample)
+
+
+(defn- rs-create
+  "Data structure allowing selection of a uniformly-likely k-sample of an
+  n-element list in O(n) time and space. Specified in Algorithm R of Vitter
+  1985.
+
+  `size` metadatum reflects total capacity of sampler; `n` reflects the current
+  number of times `insert` has been called.
+
+  NOTE: I couldn't find standard library to actually do reservoir sampling, so
+  I modified the Apache-licensed code I found by Adam Ashenfelter:
+  https://gist.github.com/ashenfad/2969087"
+  [size]
+  (with-meta [] {:size size :n 0}))
+
+(defn- rs-insert
+  "Inserts value into reservoir sampler (created using `rs-create`)."
+  [sampler v]
+  (let [{:keys [size n]} (meta sampler)
+        n' (inc n)
+        index (rand-int n')]
+    (with-meta (cond (< n' size)    (conj sampler v)
+                     (= n' size)    (shuffle (conj sampler v))
+                     (< index size) (assoc sampler index v)
+                     :else          sampler)
+      {:size size
+       :n n'})))
+
+(defn uniform-k-sample
+  "Takes a uniformly-likely k-sample of some n-element list in O(n) time/space"
+  [arr k]
+  (reduce rs-insert (rs-create k) arr))
