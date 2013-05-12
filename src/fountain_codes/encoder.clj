42b16420508e6c9e45f24ba542a8a3de1deec7be
Alex Clemmer <clemmer.alexander@gmail.com>
2013-05-12 13:26:04 -0600

    Make some functions private, fix small mistake of not using parameters
    Specifically, `encode` would take parameters telling how big to make the
    packets and where to get the data, but failed to use them.

--- src/fountain_codes/encoder.clj
+++ src/fountain_codes/encoder.clj
@@ -1,14 +1,14 @@
 (ns fountain-codes.encoder)
 
 ;; Chooses degree of packet from distribution \rho
-(defn choose-degree [k]
+(defn- choose-degree [k]
   (rand-int k))
 
 ;; filename -> packet size -> `(data k)
 ;; Generates params required to specify fountain. That is, gets data at
 ;; filename, breaks it into a list of size-l packets, and returns
 ;; `(data k), where k is the number of l-byte packets in the data.
-(defn specify-fntn [fname l]
+(defn- specify-fntn [fname l]
   (let [txt  (slurp fname)
         data (into-array (partition-all l txt))
         k    (/ (.length txt) l)]
@@ -17,7 +17,7 @@
 ;; filename -> packet size -> packets
 ;; generates enough packets to reconstruct the original file
 (defn encode [fname l]
-  (let [specs (specify-fntn "data/rfc1951" 15)
+  (let [specs (specify-fntn fname l)
         data  (first specs)
         k     (second specs)]
     (println (aget data 1))))
