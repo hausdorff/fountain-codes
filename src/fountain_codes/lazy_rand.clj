f3968e0ba3827b80da99054fd280e4401f743a59
Alex Clemmer <clemmer.alexander@gmail.com>
2013-05-14 00:20:09 -0600

    Add temporary random number generators (to be replaced later)

--- src/fountain_codes/lazy_rand.clj
+++ src/fountain_codes/lazy_rand.clj
@@ -6,3 +6,13 @@
   [seed k]
   (let [r (Random. seed)]
     (repeatedly #(+ 1 (.nextInt r k)))))
+
+;; TODO: change me
+(def renc (Random. 13))
+(def rdec (Random. 13))
+
+;; TODO: change me
+(defn enc-rand-int [k]
+  (+ 1 (.nextInt renc k)))
+(defn dec-rand-int [k]
+  (+ 1 (.nextInt dec k)))
