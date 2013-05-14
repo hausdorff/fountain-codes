4859576634ca8f62940cbe6c9a47ca9ef3b6a0e9
Alex Clemmer <clemmer.alexander@gmail.com>
2013-05-14 00:47:32 -0600

    Add non-lazy random number code (to be replaced later)

--- src/fountain_codes/lazy_rand.clj
+++ src/fountain_codes/lazy_rand.clj
@@ -8,11 +8,17 @@
     (repeatedly #(+ 1 (.nextInt r k)))))
 
 ;; TODO: change me
-(def renc (Random. 13))
-(def rdec (Random. 13))
+(def renc-deg (Random. 13))
+(def renc-pkts (Random. 27))
+(def rdec-deg (Random. 13))
+(def rdec-pkts (Random. 27))
 
 ;; TODO: change me
-(defn enc-rand-int [k]
-  (+ 1 (.nextInt renc k)))
-(defn dec-rand-int [k]
-  (+ 1 (.nextInt dec k)))
+(defn enc-deg [k]
+  (+ 1 (.nextInt renc-deg k)))
+(defn enc-pkts [k]
+  (+ 1 (.nextInt renc-pkts k)))
+(defn dec-deg [k]
+  (+ 1 (.nextInt rdec-deg k)))
+(defn dec-pkts [k]
+  (+ 1 (.nextInt rdec-pkts k)))
