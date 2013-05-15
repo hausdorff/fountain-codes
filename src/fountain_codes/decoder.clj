3617e604a53aa719801722b24060e3825240278f
Alex Clemmer <clemmer.alexander@gmail.com>
2013-05-15 13:54:03 -0600

    Add more convenience functions to decoder

--- src/fountain_codes/decoder.clj
+++ src/fountain_codes/decoder.clj
@@ -2,8 +2,7 @@
   "Implements decoding step for vanilla LT fountain codes"
   (:use [fountain-codes.lazy-rand :as lazy-rand]
         [fountain-codes.sample :as sample]
-        [clojure.core.match :only (match)])
-  (:import [java.util PriorityQueue]))
+        [clojure.core.match :only (match)]))
 
 
 (defn- pkt-deg
@@ -24,20 +23,34 @@
   [pkt-idx decd]
   (contains? decd pkt-idx))
 
-(defn src-pkt-dependencies
+(defn src-pkt-deps
   "Returns the list of encoded packets used the packet at `pkt-idx` in the
   encoding step, or nil if there are none"
   [pkt-idx deps]
   (get deps pkt-idx))
+
+(defn num-src-pkt-deps
+  "Returns number of `src-pkt-deps` for packet at `pkt-idx`"
+  [pkt-idx deps]
+  (.size (src-pkt-deps pkt-idx deps)))
+
+(defn decode-loop
+  [pkt-strm unresolved decd deps curr-msg]
+  (cond
+   (= 0 unresolved) (map (fn [s] (apply str s)) (repeat 3 (repeat 3 \c)))
+   :else (do
+           (println "DECODE LOGIC GOES HERE"))
+         ))
 
 (defn decode
   "Takes a stream of packets and the packet size, returns a decoded file"
   [pkt-strm]
   ; TODO: replace this with logging
   (println "ENCODING DATA")
-  (let [{:keys [k]} (meta pkt-strm)
-        init-pkts   (take 3000 pkt-strm)
-        q           (PriorityQueue.)
-        decd        {}]
+  (let [{:keys [k l]} (meta pkt-strm)
+        decd          #{}  ; #{indices of decoded pkts}
+        deps          {}   ; src pkt idx -> #{encoded pkt dependencies}
+        blnk-msg      (repeat k (repeat l \space))
     (println "DECODING DATA")
-    (println k)))
+    (println k)
+    (decode-loop pkt-strm k decd deps blnk-msg)))
